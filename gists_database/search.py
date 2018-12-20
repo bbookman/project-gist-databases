8e7d50bfec74cf993c44a5b509256d380b853108
bbookman <bruce.bookman@gmail.com>
2018-12-20 19:11:26 +0000

    search is failing test_search_with_github_id

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,20 @@
 from .models import Gist
+import pdb
 
 def search_gists(db_connection, **kwargs):
-    pass
+    results = []
+    
+    query = 'SELECT * from gists'
+    if not kwargs:
+        query = query
+    elif kwargs == 'github_id':
+        query += 'WHERE github_id = :github_id'
+    elif kwargs == 'created_at':
+        query += 'WHERE created_at = :created_at'
+    data = db_connection.execute(query)
+    for gist in data:
+        results.append(Gist(gist))
+        #pdb.set_trace()
+        print(str(Gist(gist)))
+    return results
+    
