bfd01b1453a79dfd504a26607ff07d28c2238c88
bbookman <bruce.bookman@gmail.com>
2018-12-20 04:46:23 +0000

    importer code complete

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -11,38 +11,30 @@ def import_gists_to_database(db, username, commit=True):
     print(data)
     for gist in data:
         params = {
-            'github_id' = gist['id'],
-            'html_url' = gist['html_url'],
-            'git_pull_url' = gist['git_pull_url'],
-            'git_push_url' = gist['git_push_url'],
-            'commits_url' = gist['commits_url'],
-            'forks_url' = gist['forks_url'],
-            'public' = gist['public'],
-            'created_at' = gist['created_at'],
-            'updated_at' = gist['updated_at'],
-            'comments' = gist['comments'],
-            'comments_url' = gist['comments_url']
+            'github_id': gist['id'],
+            'html_url' : gist['html_url'],
+            'git_pull_url': gist['git_pull_url'],
+             'git_push_url': gist['git_push_url'],
+            'commits_url': gist['commits_url'],
+            'forks_url': gist['forks_url'],
+            'public': gist['public'],
+            'created_at': gist['created_at'],
+            'updated_at': gist['updated_at'],
+            'comments': gist['comments'],
+          'comments_url': gist['comments_url']
         }
-    
-
-'''
-id INTEGER PRIMARY KEY autoincrement,
-  github_id TEXT NOT NULL,
-  html_url TEXT NOT NULL,
-
-  git_pull_url TEXT NOT NULL,
-  git_push_url TEXT NOT NULL,
-
-  commits_url TEXT NOT NULL,
-  forks_url TEXT NOT NULL,
-
-  public BOOLEAN NOT NULL,
-
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL,
-
-  comments INTEGER NOT NULL,
-  comments_url TEXT NOT NULL
-
-request.args.get('language') or request.args['language'].
-'''
+        
+        INSERT_INTO_GIST = """INSERT INTO gists (
+            "github_id", "html_url", "git_pull_url",
+            "git_push_url", "commits_url",
+            "forks_url", "public", "created_at",
+            "updated_at", "comments", "comments_url"
+        ) VALUES (
+            :github_id, :html_url, :git_pull_url,
+            :git_push_url, :commits_url, :forks_url,
+            :public, :created_at, :updated_at,
+            :comments, :comments_url
+        );"""
+        
+        if commit:
+            db.commit()
